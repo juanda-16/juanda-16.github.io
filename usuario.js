72f8d1927f90cd145970e1d8e6deaa21e51e9696
juanda-16 <juan.pizou@cun.edu.co>
2024-11-15 22:12:27 -0500

    usuario.js

--- usuario.js
+++ usuario.js
@@ -1,16 +1,15 @@
-let inicio = ['juan.pizo.ueb@gmail.com'];
-let password = ['123456789'];
+let inicio = ['juan.pizo.ueb@gmail.com']; //hacemos el arreglo unidimensional guardamos los datos asignados
+let password = ['1033098102'];
 
 function cargar(){
-    let usuario=document.getElementById("correo").value;
+    let usuario=document.getElementById("correo").value; //creamos variables y los llamamos desde el id del html para que tomen la funcion de esa variable
     let contra=document.getElementById("contraseña").value;
-
+//utilizamos la setencia para confirmar si se cumple o no 
     if(usuario == inicio && contra == password)
     {
-        window.location="mapeo.html";
+        window.location="mapeo.html";//lo redirige a la pagina cuando hay usuario creado
     }
     else
     {
-        alert("DATOS ERRONEOS")
+        alert("DATOS ERRONEOS")//mensaje de alerta cuando el usuario o persona no ingresa los datos correctos
     }
-}
